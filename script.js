08a1471727cdee4aac4230ef9dd7442137286670
Samuel Almeida <barbosasamuel24@gmail.com>
2022-01-26 23:05:03 -0300

    feat(script.js): showing weather icon accordding to the weather condition

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ locationBtn.addEventListener("click", () => {
 });
 
 function requestApi(city) {
-  let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=fc5852b1dfd144fe4c48e2ed3b72bdfb
+  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=fc5852b1dfd144fe4c48e2ed3b72bdfb
   `;
   fetchData();
 }
@@ -40,6 +40,60 @@ function onError(error) {
   infoTxt.classList.add("error");
 }
 
-function weatherDetails(info) {
-  console.log(info);
+function fetchData() {
+  infoTxt.innerText = "Getting weather details...";
+  infoTxt.classList.add("pending");
+  // getting api response and returning it with parsing into js obj and in another
+  // then function calling weatherDetails function with passing api result as an argument
+  fetch(api)
+    .then((res) => res.json())
+    .then((result) => weatherDetails(result))
+    .catch(() => {
+      infoTxt.innerText = "Something went wrong";
+      infoTxt.classList.replace("pending", "error");
+    });
 }
+
+function weatherDetails(info) {
+  if (info.cod == "404") {
+    infoTxt.classList.replace("pending", "error");
+    infoTxt.innerText = `${inputField.value} isn't a valid city name`;
+  } else {
+    // let's get required properties value from the info object
+    const city = info.name;
+    const country = info.sys.country;
+    const { description, id } = info.weather[0];
+    const { temp, feels_like, humidity } = info.main;
+    if (id == 800) {
+      wIcon.src = "icons/clear.svg";
+    } else if (id >= 200 && id <= 232) {
+      wIcon.src = "icons/storm.svg";
+    } else if (id >= 600 && id <= 622) {
+      wIcon.src = "icons/snow.svg";
+    } else if (id >= 701 && id <= 781) {
+      wIcon.src = "icons/haze.svg";
+    } else if (id >= 801 && id <= 804) {
+      wIcon.src = "icons/cloud.svg";
+    } else if ((id >= 500 && id <= 531) || (id >= 300 && id <= 321)) {
+      wIcon.src = "icons/rain.svg";
+    }
+
+    // let's pass these values to a particular html element
+
+    weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
+    weatherPart.querySelector(".weather").innerText = description;
+    weatherPart.querySelector(
+      ".location span"
+    ).innerText = `${city}, ${country}`;
+    weatherPart.querySelector(".temp .numb-2").innerText =
+      Math.floor(feels_like);
+    weatherPart.querySelector(".humidity span").innerText = `${humidity}%`;
+    infoTxt.classList.remove("pending", "error");
+    infoTxt.innerText = "";
+    inputField.value = "";
+    wrapper.classList.add("active");
+  }
+}
+arrowBack.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
