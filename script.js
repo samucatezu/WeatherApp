bb9a2297291df1f7a982ee9420d36f587c71a553
Samuel Almeida <barbosasamuel24@gmail.com>
2022-01-26 22:11:38 -0300

    feat(script.js): Getting Weather details of the User entered  city

--- script.js
+++ script.js
@@ -16,8 +16,20 @@ inputField.addEventListener("keyup", (e) => {
   }
 });
 
+locationBtn.addEventListener("click", () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  } else {
+    alert("Your browser not support geolocation api");
+  }
+});
+
 function requestApi(city) {
-  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=fc5852b1dfd144fe4c48e2ed3b72bdfb
+  let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=fc5852b1dfd144fe4c48e2ed3b72bdfb
   `;
   fetchData();
+}
+
+function weatherDetails(info) {
+  console.log(info);
 }
